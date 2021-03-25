48028e28348418b1f694f66b308d61cb79774d3f
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2021-03-25 17:17:05 -0300

    first commit

---
+++ src/Mesh/gmsh.jl
@@ -0,0 +1,16 @@
+using GmshSDK
+
+gmsh.initialize()
+gmsh.option.setNumber("Mesh.MshFileVersion", 2.2)
+gmsh.model.add("model_name")
+
+#occ:  Open Cascade backend
+tag = gmsh.model.occ.addSphere(0, 0, 0, 1)  
+gmsh.model.occ.synchronize()
+
+gmsh.model.addPhysicalGroup(2, [tag])  # dimension, [tags]
+gmsh.model.mesh.generate(2)  # dimension
+gmsh.write("file.msh")
+
+
+gmsh.finalize()
