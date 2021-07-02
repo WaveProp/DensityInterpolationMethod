0b9b8cf508a2e85f8b05c477673a5b765cc51655
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2021-07-01 20:33:47 -0400

    - single_doublelayer_dim now is working

--- src/MaxwellDIM/dim_waveprop.jl
+++ src/MaxwellDIM/dim_waveprop.jl
@@ -11,32 +11,31 @@ function get_single_and_double_layer_operators(gquad, k)
             yi, _, _, ni = get_qnode_data(qnode_i)
             S[i, j] = wj*single_layer_kernel(yi, yj, k, ni)  
             D[i, j] = wj*double_layer_kernel(yi, yj, k, ni)  
+        end
     end
     return S, D
 end
 
-function get_lebedev_sources(gquad, n_src)
+function get_lebedev_sources(gquad, n_src; kfactor=5)
     # compute source points
     _, bbox_center, bbox_radius = compute_bounding_box(gquad)
-    src_radius = r * bbox_radius
+    src_radius = kfactor * bbox_radius
     src_list = get_sphere_sources_lebedev(n_src, src_radius, bbox_center)
     return src_list
 end
 
 function get_basis_dim(gquad, k, n_src)
     function γₒ(qnode, src)
-        # Jᵗγ₀G, size=2×3
-        x, _, jac, n = get_qnode_data(qnode)
-        return transpose(jac)*single_layer_kernel(x, src, k, n) 
+        x, _, _, n = get_qnode_data(qnode)
+        return single_layer_kernel(x, src, k, n) 
     end
     function γ₁(qnode, src)
-        # Jᵗ(-n x γ₁G), size=2×3
-        x, _, jac, n = get_qnode_data(qnode)
-        return -transpose(jac)*cross_product_matrix(n)*double_layer_kernel(x, src, k, n)  
+        x, _, _, n = get_qnode_data(qnode)
+        return double_layer_kernel(x, src, k, n)  
     end
     src_list = get_lebedev_sources(gquad, n_src) # list of Lebedev sources
-    basis     = [(qnode) -> γₒ(qnode, src) for src in src_list]
-    γ₁_basis  = [(qnode) -> γ₁(qnode, src) for src in src_list]
+    basis = [(qnode) -> γₒ(qnode, src) for src in src_list]
+    γ₁_basis = [(qnode) -> γ₁(qnode, src) for src in src_list]
     return basis, γ₁_basis
 end
 
@@ -44,8 +43,8 @@ function get_auxiliary_quantities_dim(gquad,Sop,Dop,basis,γ₁_basis)
     n_src = length(basis)
     # compute matrix of basis evaluated on Y
     ynodes = get_qnodes(gquad)
-    γ₀B = Matrix{T}(undef, length(ynodes), n_src)
-    γ₁B = Matrix{T}(undef, length(ynodes), n_src)
+    γ₀B = Matrix{MaxwellKernelType}(undef, length(ynodes), n_src)
+    γ₁B = Matrix{MaxwellKernelType}(undef, length(ynodes), n_src)
     for k in 1:n_src
         for i in 1:length(ynodes)
             γ₀B[i,k] = basis[k](ynodes[i])
@@ -53,22 +52,52 @@ function get_auxiliary_quantities_dim(gquad,Sop,Dop,basis,γ₁_basis)
         end
     end
     # integrate the basis over Y
-    R = Op1*γ₁B - Op2*γ₀B
+    R = -0.5*γ₀B - Dop*γ₀B - Sop*γ₁B
     return γ₀B, γ₁B, R
 end
 
-function single_doublelayer_dim(gquad::GlobalQuadrature)
-    k = 2.0
-    n_src = 26
-    
+function get_singular_weights_dim(gquad,γ₀B,γ₁B,R)
+    n_src = size(γ₀B,2)
+    T = MaxwellKernelType
+    Is = Int[]
+    Js = Int[]
+    Ss = T[]   # for single layer
+    Ds = T[]   # for double layer
+    num_els = get_number_of_elements(gquad)
+    # FIXME: assumes all elements are equal
+    n_qnodes_per_element = length(first(gquad.elements))
+    M = Matrix{T}(undef,2*n_qnodes_per_element,n_src)
+    for n in 1:num_els
+        j_glob = get_inelement_qnode_indices(gquad, n)
+        M[1:n_qnodes_per_element,:]     = @view γ₀B[j_glob,:]
+        M[n_qnodes_per_element+1:end,:] = @view γ₁B[j_glob,:]
+        F = pinv(blockmatrix_to_matrix(M))  # FIXME: Change pseudoinverse for LQ
+        for i in j_glob
+            tmp_scalar  = blockmatrix_to_matrix(R[i:i,:]) * F
+            tmp = matrix_to_blockmatrix(tmp_scalar,T)
+            Dw = view(tmp,1:n_qnodes_per_element)
+            Sw = view(tmp,(n_qnodes_per_element+1):(2*n_qnodes_per_element))
+            #w = axpby!(a,view(tmp,1:n_qnodes),b,view(tmp,(n_qnodes+1):(2*n_qnodes)))
+            append!(Is,fill(i,n_qnodes_per_element))
+            append!(Js,j_glob)
+            append!(Ss,Sw)
+            append!(Ds,Dw)
+        end
+    end
+    n_qnodes = get_number_of_qnodes(gquad)
+    Sp = sparse(Is,Js,Ss,n_qnodes,n_qnodes)
+    Dp = sparse(Is,Js,Ds,n_qnodes,n_qnodes)
+    return Sp, Dp
+end
+
+function single_doublelayer_dim(gquad::GlobalQuadrature; k, n_src)
     S, D = get_single_and_double_layer_operators(gquad, k)
     # precompute dim quantities
      # list of functions γ₀ and γ₁ for each source
     basis, γ₁_basis = get_basis_dim(gquad, k, n_src) 
-    γ₀B, γ₁B, R = get_auxiliary_quantities_dim(gquad,Sop,Dop,basis,γ₁_basis)
+    γ₀B, γ₁B, R = get_auxiliary_quantities_dim(gquad,S,D,basis,γ₁_basis)
     # compute corrections
-    δS = _singular_weights_dim(Sop,γ₀B,γ₁B,R,dict_near)
-    δD = _singular_weights_dim(Dop,γ₀B,γ₁B,R,dict_near)
+    δS, δD = get_singular_weights_dim(gquad,γ₀B,γ₁B,R)
     # add corrections to the dense part
     axpy!(true,δS,S)  # S = S + δS
     axpy!(true,δD,D)  # D = D + δD
