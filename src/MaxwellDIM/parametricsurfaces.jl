2d7466b8df01f5209383be75cd59012e72616b14
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2021-07-25 20:07:32 -0400

    fix nystrom_gquad in parametricsurfaces.jl

--- src/MaxwellDIM/parametricsurfaces.jl
+++ src/MaxwellDIM/parametricsurfaces.jl
@@ -1,13 +1,24 @@
 function generate_gquad(mesh)
     @assert length(mesh.elt2dof) == 1  # single type of element
     old_elements = first(values(mesh.elt2dof))
-    qnodes = [nystromdof2qnode(dof) for dof in Nystrom.dofs(mesh)]
     elements = [element2element(col) for col in eachcol(old_elements)]
+    qnodes = generate_qnodes(mesh, elements)
     return GlobalQuadrature(qnodes, elements)
 end
-function nystromdof2qnode(dof)
-    index = 0
+function generate_qnodes(mesh, elements)
     element_index = 0
+    dofs = Nystrom.dofs(mesh)
+    qnodes = QNode[]
+    for el in elements
+        element_index += 1
+        for index in el 
+            qnode = nystromdof2qnode(dofs[index], index, element_index)
+            push!(qnodes, qnode)
+        end
+    end
+    return qnodes
+end
+function nystromdof2qnode(dof, index, element_index)
     qnode = Nystrom.coords(dof)
     weigth = Nystrom.weight(dof)
     jacobian = Nystrom.jacobian(dof)
